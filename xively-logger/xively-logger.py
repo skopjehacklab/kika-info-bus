6e0b7557fdd00771481a0b6229b61ee347f5f561
Georgi Stanojevski <glisha@gmail.com>
2014-04-24 16:03:55 +0200

    Initial commit for a new module to upload readouts to Xively.
    At the moment uploads only the Open/Closed status. Runs through uwsgi.cron every 5 minutes.

---
+++ xively-logger/xively-logger.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2.7
+# -*- encoding: utf-8 -*-
+import zmq
+import requests
+import json
+import os, re, datetime, ConfigParser
+
+
+config = ConfigParser.RawConfigParser()
+config.read(os.environ['CONFIG_FILE'])
+
+
+ctx = zmq.Context()
+
+def get_value(msg):
+    match = re.search(r'\b(OPEN|CLOSED)\b', msg)
+    return match.group(1)
+
+def get_temperature(msg):
+    return false
+
+def main():
+    socket = ctx.socket(zmq.SUB)
+    socket.setsockopt(zmq.SUBSCRIBE, '')
+    socket.connect(config.get('zmq', 'publisher_addr'))
+
+    url="http://api.xively.com/v2/feeds/%s/datastreams/%s/datapoints" % (config.get('xively','feed_id'),"hacklab_status")
+    headers = {'X-ApiKey':config.get('xively','api_key')}
+
+    msg = socket.recv()
+    current_value = get_value(msg)
+    now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ%z')
+        
+    if current_value == 'OPEN':
+        payload={'datapoints': [{'at': now, 'value': 1}]}
+    if current_value == 'CLOSED':
+        payload={'datapoints': [{'at': now, 'value': 0}]}
+
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    print url
+    print payload
+
+if __name__ == '__main__':
+    main()
