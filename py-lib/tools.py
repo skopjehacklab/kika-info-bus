92e0c4a9204bd38b33d6dad3f4f42f64fd0ac640
Georgi Stanojevski <glisha@gmail.com>
2014-04-29 15:34:28 +0200

    Added py-lib/tools.py for common tools used by all modules

---
+++ py-lib/tools.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2.7
+# -*- encoding: utf-8 -*-
+
+import json
+
+def get_temperature(msg,json=False):
+    """Parses the arduino output and returns a data structure with sensorid to name mapings.
+
+    Or the same data structure as json.
+    """
+    #sensorid to name mapping
+    sensors = {
+            "28B535930013": {
+                                "name": "hardware_room",
+                                "value": "", "readout_time": "", "current_millis": ""
+                            },
+            "284C606340081": {
+                                "name": "lounge_area",
+                                "value": "", "readout_time": "", "current_millis": ""
+                            },
+            "285BEF57300C7": {
+                                "name": "random_room",
+                                "value": "", "readout_millis": "", "current_millis": ""
+                            },
+            "282576B0300C7": {
+                                "name": "outside",
+                                "value": "", "readout_time": "", "current_millis": ""
+                            },
+    }
+
+    for line in msg.splitlines()[:-1]:
+        sensor_addr,curr_temp,readout_millis,current_millis = line.split(",")
+
+        try:
+            sensors[sensor_addr]['value'] = curr_temp
+            sensors[sensor_addr]['readout_millis'] = readout_millis
+            sensors[sensor_addr]['current_millis'] = current_millis
+        except KeyError:
+            print "Unknown sensor found %s" % sensor_addr
+            continue
+
+        if int(current_millis)-int(readout_millis)>300000:
+            print "Haven't read new reading from %s for over 5 minutes." % datapoint_id
+            continue
+
+    if json:
+        return json.dumps(sensors)
+
+    return sensors
+
--- xively-logger/xively-logger.py
+++ xively-logger/xively-logger.py
@@ -5,6 +5,7 @@ import requests
 import json
 import os, re, datetime, ConfigParser
 
+from tools import get_temperature
 
 config = ConfigParser.RawConfigParser()
 config.read(os.environ['CONFIG_FILE'])
@@ -15,44 +16,6 @@ ctx = zmq.Context()
 def get_status(msg):
     match = re.search(r'\b(OPEN|CLOSED)\b', msg)
     return match.group(1)
-
-def get_temperature(msg):
-    #sensorid to name mapping
-    sensors = {
-            "28B535930013": {  
-                                "name": "hardware_room", 
-                                "value": "", "readout_time": "", "current_millis": "" 
-                            },
-            "284C606340081": { 
-                                "name": "lounge_area", 
-                                "value": "", "readout_time": "", "current_millis": "" 
-                            },
-            "285BEF57300C7": { 
-                                "name": "random_room", 
-                                "value": "", "readout_millis": "", "current_millis": "" 
-                            },
-            "282576B0300C7": { 
-                                "name": "outside", 
-                                "value": "", "readout_time": "", "current_millis": "" 
-                            },
-    }
-
-    for line in msg.splitlines()[:-1]: 
-        sensor_addr,curr_temp,readout_millis,current_millis = line.split(",")
-
-        try:
-            sensors[sensor_addr]['value'] = curr_temp
-            sensors[sensor_addr]['readout_millis'] = readout_millis
-            sensors[sensor_addr]['current_millis'] = current_millis
-        except KeyError:
-            print "Unknown sensor found %s" % sensor_addr
-            continue
-
-        if int(current_millis)-int(readout_millis)>300000:
-            print "Haven't read new reading from %s for over 5 minutes." % datapoint_id
-            continue
-
-    return sensors
 
 def main():
     socket = ctx.socket(zmq.SUB)
