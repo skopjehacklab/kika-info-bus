e0dabb45d62086c1616b802c1bab1f0ca55daad5
Damjan Georgievski <gdamjan@gmail.com>
2014-04-29 17:26:21 +0200

    implement longpoll
    When the url /longpoll is requested, the request greenlet creates an event object,
    and wait until the event is set to respond. The event object is set by the background
    greenlet that is subscribed to the zmq publisher.
    
    The event object is put in a global queue, and is removed from the queue automatically
    when a message arrives and the queue is iterated.

--- http-publisher/publish-to-web.py
+++ http-publisher/publish-to-web.py
@@ -4,6 +4,7 @@
 from flask import Flask, Response
 import zmq.green as zmq
 import gevent
+import gevent.queue
 import os, ConfigParser
 
 config = ConfigParser.RawConfigParser()
@@ -16,6 +17,7 @@ app = Flask(__name__)
 @app.before_first_request
 def run():
     app.latest_message = None
+    app.waiters_queue = gevent.queue.Queue()
     def _process():
         socket = ctx.socket(zmq.SUB)
         socket.setsockopt(zmq.SUBSCRIBE, '')
@@ -23,12 +25,22 @@ def run():
         while True:
             msg = socket.recv()
             app.latest_message = msg
+            # wake up all the waiting requests
+            for ev in app.waiters_queue:
+                ev.set()
 
     gevent.spawn(_process)
 
 
 @app.route('/')
 def index():
+    return Response(app.latest_message, content_type='text/plain; charset=utf-8')
+
+@app.route('/longpoll')
+def longpoll():
+    ev = gevent.event.Event()
+    app.waiters_queue.put(ev)
+    ev.wait()
     return Response(app.latest_message, content_type='text/plain; charset=utf-8')
 
 if __name__ == '__main__':
