292ae2f4ce01694f57992369cc4f24a16fc01be0
Georgi Stanojevski <glisha@gmail.com>
2015-12-09 20:54:31 +0100

    Push OPEN/CLOSED status in the InfluxDB.
    Updated the example uwsgi.ini with influxdb cronjobs.

---
+++ influxdb-logger/status.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.7
+# -*- encoding: utf-8 -*-
+import zmq
+from influxdb import InfluxDBClient
+import json
+import os, re, datetime, ConfigParser
+
+from tools import get_temperature, get_status
+
+config = ConfigParser.RawConfigParser()
+config.read(os.environ['CONFIG_FILE'])
+
+
+ctx = zmq.Context()
+
+def main():
+    socket = ctx.socket(zmq.SUB)
+    socket.setsockopt(zmq.SUBSCRIBE, '')
+    socket.connect(config.get('zmq', 'publisher_addr'))
+
+    dsn = config.get('influxdb', 'database')
+    client = InfluxDBClient.from_DSN(dsn, timeout=5)
+
+    msg = socket.recv()
+    current_status = get_status(msg)
+
+    now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ%z')
+    data = [
+        {
+            "measurement": "doorstatus",
+            "time": now,
+            "tags": {
+                "location": "hacklab"
+            },
+            "fields": { "doorstatus": current_status }
+        }
+    ]
+
+    client.write_points(data)
+
+if __name__ == '__main__':
+    main()
+
